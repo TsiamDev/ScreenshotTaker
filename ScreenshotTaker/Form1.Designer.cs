3fe8ff7660485f5d985746fa09137e5ba43a3ab8
Tsiamitros <kos.tsiamitros@gmail.com>
2020-10-27 11:06:06 +0200

    Initial commit

--- ScreenshotTaker/Form1.Designer.cs
+++ ScreenshotTaker/Form1.Designer.cs
@@ -28,13 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pickDirButton = new System.Windows.Forms.Button();
+            this.ssLabel = new System.Windows.Forms.Label();
+            this.lastSSLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // pickDirButton
+            // 
+            this.pickDirButton.Location = new System.Drawing.Point(12, 28);
+            this.pickDirButton.Name = "pickDirButton";
+            this.pickDirButton.Size = new System.Drawing.Size(102, 23);
+            this.pickDirButton.TabIndex = 0;
+            this.pickDirButton.Text = "Choose Directory";
+            this.pickDirButton.UseVisualStyleBackColor = true;
+            this.pickDirButton.Click += new System.EventHandler(this.pickDirButton_Click);
+            // 
+            // ssLabel
+            // 
+            this.ssLabel.AutoSize = true;
+            this.ssLabel.Location = new System.Drawing.Point(12, 9);
+            this.ssLabel.Name = "ssLabel";
+            this.ssLabel.Size = new System.Drawing.Size(209, 13);
+            this.ssLabel.TabIndex = 1;
+            this.ssLabel.Text = "Pick a directory to start saving screenshots";
+            // 
+            // lastSSLabel
+            // 
+            this.lastSSLabel.AutoSize = true;
+            this.lastSSLabel.Location = new System.Drawing.Point(12, 58);
+            this.lastSSLabel.Name = "lastSSLabel";
+            this.lastSSLabel.Size = new System.Drawing.Size(0, 13);
+            this.lastSSLabel.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(831, 99);
+            this.Controls.Add(this.lastSSLabel);
+            this.Controls.Add(this.ssLabel);
+            this.Controls.Add(this.pickDirButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button pickDirButton;
+        private System.Windows.Forms.Label ssLabel;
+        private System.Windows.Forms.Label lastSSLabel;
     }
 }
 
--- ScreenshotTaker/Form1.cs
+++ ScreenshotTaker/Form1.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Gma.System.MouseKeyHook;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +15,102 @@ namespace ScreenshotTaker
 {
     public partial class Form1 : Form
     {
+        #region Declarations
+        private IKeyboardMouseEvents m_GlobalHook;
+        private FolderBrowserDialog fbd;
+
+        public int screenWidth;
+        public int screenHeight;
+
+        public string ssDirPath;
+
+        #endregion
+
+        #region Initialization
         public Form1()
         {
             InitializeComponent();
+            Subscribe();
+
+            fbd = new FolderBrowserDialog();
+
+            ssDirPath = null;
+            screenWidth = Screen.PrimaryScreen.Bounds.Width;
+            screenHeight = Screen.PrimaryScreen.Bounds.Height;
         }
+        #endregion
+
+        #region Sample Code for key presses Modified from: https://github.com/gmamaladze/globalmousekeyhook/blob/vNext/examples/FormsExample/Sample.cs
+        public void Subscribe()
+        {
+            m_GlobalHook = Hook.GlobalEvents();
+
+            m_GlobalHook.KeyPress += GlobalHookKeyPress;
+        }
+
+        private void GlobalHookKeyPress(object sender, KeyPressEventArgs e)
+        {
+            Console.WriteLine("KeyPress: \t{0}", e.KeyChar);
+            HandleKeyPressed(e.KeyChar);
+        }
+        #endregion
+
+        #region keyPressed Handler
+        public void HandleKeyPressed(char keyCode)
+        {
+            if (keyCode == 's')
+            {
+                if (ssDirPath == null)
+                {
+                    SelectDir(ref ssLabel, "Hit 's' to save screenshot to the specified folder.", ref ssDirPath);
+                }
+                string path = SaveScreenshot();
+                lastSSLabel.Text = "Last File Created: " + path;
+            }
+        }
+        #endregion
+
+        #region Screenshot
+        private void pickDirButton_Click(object sender, EventArgs e)
+        {
+            SelectDir(ref ssLabel, "Hit 's' to save screenshot to the specified folder.", ref ssDirPath);
+        }
+
+        private void SelectDir(ref Label label, string str, ref string reference)
+        {
+            DialogResult result = fbd.ShowDialog();
+
+            if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+            {
+                string path = fbd.SelectedPath;
+                path = path.Replace("\\", "/");
+                path += "/";
+                reference = path;
+
+                label.Text = str;
+            }
+        }
+
+        public string SaveScreenshot()
+        {
+            string name = null;
+            byte[] buffer = new byte[screenWidth * screenHeight];
+            Bitmap bitmap = new Bitmap(screenWidth, screenHeight);
+            using (var graphics = Graphics.FromImage(bitmap))
+            {
+                graphics.CopyFromScreen(0, 0, 0, 0, new System.Drawing.Size(screenWidth, screenHeight));
+                var bits = bitmap.LockBits(new Rectangle(0, 0, screenWidth, screenHeight), ImageLockMode.ReadOnly, PixelFormat.Format32bppRgb);
+                Marshal.Copy(bits.Scan0, buffer, 0, buffer.Length);
+                bitmap.UnlockBits(bits);
+
+                name = DateTime.Now.ToString();
+                name = name.Replace("/", "-");
+                name = name.Replace(":", "-");
+                name = ssDirPath + name + ".png";
+                bitmap.Save(name);
+            }
+            return name;
+        }
+        #endregion
     }
 }
